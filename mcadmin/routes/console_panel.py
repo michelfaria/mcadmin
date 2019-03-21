0a4762f2b6653d456063c6caf31d5975f9cc0746
Michel <michelfariarj@gmail.com>
2019-03-20 22:53:42 -0300

    working eventsource

--- mcadmin/routes/console_panel.py
+++ mcadmin/routes/console_panel.py
@@ -1,5 +1,6 @@
 # mcadmin/routes/console_panel.py
 import logging
+import time
 
 from flask import render_template, Response
 from flask_login import login_required
@@ -8,6 +9,7 @@ from mcadmin.main import app
 from mcadmin.server.server import proc, is_server_running
 
 LOGGER = logging.getLogger(__name__)
+SERVER_NOT_RUNNING_ERR_CODE = 'mcadmin:err:server_not_running'
 
 
 @app.route('/console_panel')
@@ -23,16 +25,21 @@ def stream():
 
 
 def console_stream():
-    while True:
-        yield('Foo')
-    # try:
-    #     while True:
-    #         if not is_server_running():
-    #             yield 'Server is not running'
-    #             continue
-    #         assert proc is not None and proc.poll() is None
-    #         out = proc.stdout.readlines()
-    #         if len(out) > 0:
-    #             yield out
-    # except GeneratorExit as e:
-    #     LOGGER.debug('console_stream GeneratorExit: ' + str(e))
+    try:
+        while True:
+            if not is_server_running():
+                yield 'data: ' + SERVER_NOT_RUNNING_ERR_CODE + '\n\n'
+                time.sleep(10)  # Wait a little before checking again
+                continue
+
+            assert proc is not None and proc.poll() is None
+
+            out = proc.stdout.readline()
+            if len(out) > 0:
+                yield 'data: ' + out + '\n\n'
+            else:
+                # Wait a little to prevent a lot of CPU usage
+                time.sleep(1)
+    except GeneratorExit as e:
+        # Client quit console page
+        LOGGER.debug('console_stream GeneratorExit: ' + str(e))
