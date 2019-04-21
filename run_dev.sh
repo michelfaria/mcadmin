4daa58a47ae49fb85afebea9d22980f5f1ea04db
Michel <michelfariarj@gmail.com>
2019-04-21 16:47:25 -0300

    Fixed macro issue and startup script

--- run_dev.sh
+++ run_dev.sh
@@ -3,5 +3,8 @@
 # Set current working directory to this script's location
 cd "${0%/*}"
 
+# Make CSS directory
+mkdir ./mcadmin/static/css/
+
 # Run the app
 FLASK_APP=mcadmin.main FLASK_ENV=development flask run
